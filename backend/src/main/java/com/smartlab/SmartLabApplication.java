621b1cf762a402604c10857d721f9edb264e6766
datdt <2imdanton139@gmail.com>
2026-07-23 00:44:52 +0700

    Fix application entrypoint syntax

--- backend/src/main/java/com/smartlab/SmartLabApplication.java
+++ backend/src/main/java/com/smartlab/SmartLabApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-
-SmartLabApplication {
+public class SmartLabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartLabApplication.class, args);
